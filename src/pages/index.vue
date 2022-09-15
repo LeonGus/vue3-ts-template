2d7cba20a3165883eaa1d37a064a969019bad26f
Leon <leonmr@qq.com>
2022-09-15 20:31:09 +0800

    feat: 增加eslint

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,12 +1,16 @@
+<script setup lang="ts">
+const timeSite = useDateFormat(useNow(), 'YYYY-MM-DD HH:mm:ss')
+toggleDark()
+</script>
+
 <template>
-    <HelloWorld />
-    <div text-center mt-20>
-        现在是： {{timeSite}}
-    <button @click="toggleDark()">切换</button>
-    <i icon-btn i-carbon-logo-github align-text-top></i>
-    </div>
+  <HelloWorld />
+  <div text-center mt-20>
+    现在是： {{ timeSite }}
+    <button @click="toggleDark()">
+      切换
+    </button>
+    <i icon-btn i-carbon-logo-github align-text-top />
+  </div>
 </template>
 
-<script setup lang="ts">
-    let timeSite = useDateFormat(useNow(), 'YYYY-MM-DD HH:mm:ss')
-</script>
